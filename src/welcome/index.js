1aa61a2846360bcdc3e06f2971b5647ec30da844
Pius <jonzyp4reala@gmail.com>
2020-12-05 19:54:56 +0100

    Created navigation for authentications

--- src/welcome/index.js
+++ src/welcome/index.js
@@ -1,2 +1,20 @@
-export {default as Login} from './Authentication/Login';
-export {default as SignUp} from './Authentication/SignUp';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import SignUp from './Authentication/SignUp';
+import Login from './Authentication/Login';
+import ForgotPassword from './Authentication/ForgotPassword';
+import ResetPassword from './Authentication/ResetPassword';
+
+const AuthStack = createStackNavigator();
+
+export const AuthenticationNavigation = () => {
+  return (
+    <AuthStack.Navigator headerMode="none">
+      <AuthStack.Screen name="SignUp" component={SignUp} />
+      <AuthStack.Screen name="Login" component={Login} />
+      <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <AuthStack.Screen name="ResetPassword" component={ResetPassword} />
+    </AuthStack.Navigator>
+  );
+};
